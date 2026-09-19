e73426e28490c45ba2a134c69d67f62a11510a8a
Anna Liao <b89324@gmail.com>
2026-09-19 21:14:20 +0800

    Add RC3 asset: magic40-after-base.js

---
+++ magic40-after-base.js
@@ -0,0 +1,5 @@
+/* RC2 safety normalization after github-app.js. */
+if (!magic40HadPersonal) {
+  state.tasks={}; state.sessions=[]; state.exams=[]; state.calendarEvents=[]; state.magicMemory=[];
+  state.reviewCompletions={}; state.cancelledReviews={}; state.reviewTimeRecords={};
+}
